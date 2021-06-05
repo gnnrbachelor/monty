b7c0927420509966039f046f1c5b46c546664cc9
gnnrbachelor <gnnrbachelor@gmail.com>
2021-06-05 00:04:40 +0000

    Fix add

--- add.c
+++ add.c
@@ -11,9 +11,17 @@
 void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
+	stack_t *c_node = *stack;
 	unsigned int i = 0;
+	unsigned int j = 0;
 
-	if (*stack == NULL || (*stack)->next == NULL || (*stack)->next->next == NULL)
+	while (c_node)
+	{
+		j++;
+		c_node = c_node->next;
+	}
+
+	if (*stack == NULL || (*stack)->next == NULL || (*stack)->next->next == NULL || j < 2)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
